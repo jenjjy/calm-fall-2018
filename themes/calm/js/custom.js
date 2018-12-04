a2caaed9a350262f066d2c618f85a86702ae8581
Nikki Wu <nikkiwu9@gmail.com>
2018-12-04 15:34:39 -0800

    remove console.log

--- themes/calm/js/custom.js
+++ themes/calm/js/custom.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (function ($) {
     $(document).ready(function () {
 
@@ -6,6 +5,3 @@
     })
 
 });
-=======
-console.log('hey sluts');
->>>>>>> 855beeb447c1e2dceb3a4520f11f409d8b1d8f42
