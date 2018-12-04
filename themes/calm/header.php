1a67d02aea174627033ebdd1809d032362e1c722
Jennifer <jen.yiu.90@gmail.com>
2018-12-04 09:12:26 -0800

    rename all red-starter to clam

--- themes/calm/header.php
+++ themes/calm/header.php
@@ -2,7 +2,7 @@
 /**
  * The header for our theme.
  *
- * @package RED_Starter_Theme
+ * @package Calmversation_Theme
  */
 
 ?><!DOCTYPE html>
--- themes/calm/page.php
+++ themes/calm/page.php
@@ -2,7 +2,7 @@
 /**
  * The template for displaying all pages.
  *
- * @package RED_Starter_Theme
+ * @package Calmversation_Theme
  */
 
 get_header(); ?>
