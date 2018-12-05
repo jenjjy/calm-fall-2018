ef36564fd23e70528f657a7aa21fd6112177eb15
Jennifer <jen.yiu.90@gmail.com>
2018-12-04 16:05:49 -0800

    delete sidebar and searchbar

--- themes/calm/404.php
+++ themes/calm/404.php
@@ -20,7 +20,6 @@ get_header(); ?>
 				<div class="page-content">
 					<p><?php echo esc_html( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?' ); ?></p>
 
-					<?php get_search_form(); ?>
 
 					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
