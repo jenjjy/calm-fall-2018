61a0492f44297acf03707484052d60ed67bdebc6
Nikki Wu <nikkiwu9@gmail.com>
2018-12-05 10:12:03 -0800

    update mobile front-page

--- themes/calm/inc/extras.php
+++ themes/calm/inc/extras.php
@@ -11,12 +11,14 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
-function calm_body_classes( $classes ) {
-	// Adds a class of group-blog to blogs with more than 1 published author.
-	if ( is_multi_author() ) {
-		$classes[] = 'group-blog';
-	}
+function calm_body_classes($classes)
+{
+    // Adds a class of group-blog to blogs with more than 1 published author.
+    if (is_multi_author()) {
+        $classes[] = 'group-blog';
+    }
 
-	return $classes;
+    return $classes;
 }
-add_filter( 'body_class', 'calm_body_classes' );
+
+add_filter('body_class', 'calm_body_classes');
