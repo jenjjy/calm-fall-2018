f3c9043326dc8b33b830050ad13a5da232f266fd
Jennifer <jen.yiu.90@gmail.com>
2018-12-05 11:07:29 -0800

    add testimony code to frontpage

--- themes/calm/header.php
+++ themes/calm/header.php
@@ -12,7 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-
+    <link href="dist/hamburgers.css" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 
@@ -27,8 +27,11 @@
         </div><!-- .site-branding -->
 
         <nav id="site-navigation" class="main-navigation" role="navigation">
-            <button class="menu-toggle" aria-controls="primary-menu"
-                    aria-expanded="false"><?php echo esc_html('Primary Menu'); ?></button>
+        <button class="hamburger hamburger--slider is-active" type="button">
+            <span class="hamburger-box">
+                <span class="hamburger-inner"></span>
+            </span>
+        </button>  
             <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
         </nav><!-- #site-navigation -->
     </header><!-- #masthead -->
