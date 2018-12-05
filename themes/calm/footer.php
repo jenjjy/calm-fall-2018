2e5bcbccbddb5846a761ee01053804f3a6e72b95
felix <sparkyxu@hotmail.com>
2018-12-05 14:49:46 -0800

    add links and social media icon to the .site-footer

--- themes/calm/footer.php
+++ themes/calm/footer.php
@@ -7,41 +7,64 @@
 
 ?>
 
-	</div><!-- #content -->
+	<body><!-- #content -->
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
-		  <div class="site-info">
-			 <div class="socail-media-icon">
-		
-			 <a href="mailto:hello@calmversation.com">
-			      <i class="far fa-envelope"></i>
-			 </a>
+		  
+			<div class="socail-media-icon">
+				<ul>
+				  <li>  
+				       <a href="mailto:hello@calmversation.com">
+			               <i class="far fa-envelope"></i>
+			          </a>
+				  </li>
 
-			 <a href="https://www.facebook.com/calmversation/">
-                   <i class="fab fa-facebook"></i>
-			 </a>	 
-			<a href="https://www.instagram.com/calmversation/">      
-					 <i class="fab fa-instagram"></i>   
-			</a>
+			      <li>  
+				       <a href="https://www.facebook.com/calmversation/">
+                          <i class="fab fa-facebook"></i>
+			           </a>
+				 </li> 
+				  <li> 
+				    <a href="https://www.instagram.com/calmversation/">      
+					      <i class="fab fa-instagram"></i>   
+			           </a>
+				   </li>
+				<ul>
 
-			</div>
-                <div class="footer-naviation">
-					<ul>
-							<a href="https://calm.van.cp.academy.red"><li>Home</li></a>
-							<a href=""><li>faq</li></a>
-							<a href=""><li>terms of use</li></a>
-							<a href=""><li>Privacy Policy<li></a>
-					</ul>
-              </div>
+			</div><!--.social-media-icon -->
 
+            <div class="footer-navigation">
+				<ul>
+					<li>
+					    <a href="https://calm.van.cp.academy.red">
+						    Home
+						</a>
+				   </li>
+					<li>
+						<a href="https://www.indiegogo.com/projects/calmversation-a-new-model-for-learning#/faq">
+							faq
+						</a>
+					</li>
+					 <li>
+						 <a href="https://calmversation.com/terms-of-use">
+							terms of use
+						</a>
+					</li>
+					 <li>
+						 <a href="https://calmversation.com/privacy-policy">
+							 Privacy Policy
+						</a>
+					</li>
+				</ul>
+            </div><!--.footer-navigation -->
 
-
-			   		</div><!--widget-area-->
-				</div><!-- .site-info -->
+			<div class="footer-text">   
+			  <h6> 2018 Missy Jena Media Innovation Inc. Calmversation</h6> 		
+			</div>		
 		</footer><!-- #colophon -->
-	</div><!-- #page -->
+	
 
-	<?php wp_footer(); ?>
+	   <?php wp_footer(); ?>
 
 	</body>
 </html>
