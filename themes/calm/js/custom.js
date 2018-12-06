3fc568c0dadcddc63dbedf37aa50bebe474b27d4
Jennifer <jen.yiu.90@gmail.com>
2018-12-06 14:06:29 -0800

    header

--- themes/calm/js/custom.js
+++ themes/calm/js/custom.js
@@ -1,7 +1,12 @@
-(function ($) {
-    $(document).ready(function () {
-
-
-    })
-
+(function($) {
+  $(document).ready(function() {
+    // hamburger menu yum yum
+    $('.hamburger').click(function() {
+      $(this).toggleClass('open');
+    });
+    // About Us sub-menu
+    $('.sub-menu').click(function() {
+      $(this).toggleClass('open');
+    });
+  }); // end of document
 });
