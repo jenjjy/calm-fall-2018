4eca973bf00ec884972356558bf9a725b3ecd6d7
felix <sparkyxu@hotmail.com>
2018-12-06 09:43:24 -0800

    fix footer

--- themes/calm/footer.php
+++ themes/calm/footer.php
@@ -11,7 +11,31 @@
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
 		  
-			<div class="socail-media-icon">
+            <div class="footer-navigation">
+				<ul>
+					<li>
+					    <a href="https://calm.van.cp.academy.red">
+						    Home
+						</a>
+				   </li>
+					<li>
+						<a href="https://www.indiegogo.com/projects/calmversation-a-new-model-for-learning#/faq">
+							FAQ
+						</a>
+					</li>
+					 <li>
+						 <a href="https://calmversation.com/terms-of-use">
+							Terms of Use
+						</a>
+					</li>
+					<li>
+						 <a href="https://calmversation.com/privacy-policy">
+							 Privacy Policy
+						</a>
+					</li>
+				</ul>
+            </div><!--.footer-navigation -->
+			<div class="social-media-icon">
 				<ul>
 				  <li>  
 				       <a href="mailto:hello@calmversation.com">
@@ -29,38 +53,13 @@
 					      <i class="fab fa-instagram"></i>   
 			           </a>
 				   </li>
-				<ul>
+              </ul>
 
 			</div><!--.social-media-icon -->
 
-            <div class="footer-navigation">
-				<ul>
-					<li>
-					    <a href="https://calm.van.cp.academy.red">
-						    Home
-						</a>
-				   </li>
-					<li>
-						<a href="https://www.indiegogo.com/projects/calmversation-a-new-model-for-learning#/faq">
-							faq
-						</a>
-					</li>
-					 <li>
-						 <a href="https://calmversation.com/terms-of-use">
-							terms of use
-						</a>
-					</li>
-					 <li>
-						 <a href="https://calmversation.com/privacy-policy">
-							 Privacy Policy
-						</a>
-					</li>
-				</ul>
-            </div><!--.footer-navigation -->
-
-			<div class="footer-text">   
+			 
 			  <h6> 2018 Missy Jena Media Innovation Inc. Calmversation</h6> 		
-			</div>		
+					
 		</footer><!-- #colophon -->
 	
 
