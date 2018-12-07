b56a568590e6b8374a72f53aab724d6cb1e534b7
Jennifer <jen.yiu.90@gmail.com>
2018-12-07 15:16:48 -0800

    adding mobile slide menu

--- themes/calm/js/custom.js
+++ themes/calm/js/custom.js
@@ -1,8 +1,26 @@
-(function ($) {
-  $(document).ready(function () {
-    // hamburger menu yum yum
-    $('.hamburger').click(function () {
-      $(this).toggleClass('open');
-    }); //end of hamburger bun yum yum
+(function($) {
+  $(document).ready(function() {
+    // slide menu
+    let mobileMenu = $('#side-menu');
+    let mainMenu = $('#main');
+
+    function openSlideMenu() {
+      mobileMenu.style.width = '100%';
+      // mainMenu.style.marginLeft = '250px';
+    }
+
+    function closeSlideMenu() {
+      mobileMenu.style.width = '0';
+      // mainMenu.style.marginLeft = '0';
+    }
+
+    $('.open-menu').on('click', function() {
+      // event.preventDefault();
+      openSlideMenu();
+    });
+    $('.btn-close').on('click', function() {
+      // event.preventDefault();
+      closeSlideMenu();
+    });
   }); // end of document
 });
