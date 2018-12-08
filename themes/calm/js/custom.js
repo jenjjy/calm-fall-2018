d657e8226a4a6f8dbc88c4bde4a3c9ae8b0136c2
Nikki Wu <nikkiwu9@gmail.com>
2018-12-08 09:13:00 -0800

    fix custom.js file

--- themes/calm/js/custom.js
+++ themes/calm/js/custom.js
@@ -1,26 +1,25 @@
-(function($) {
-  $(document).ready(function() {
-    // slide menu
-    let mobileMenu = $('#side-menu');
-    let mainMenu = $('#main');
+(function ($) {
+    $(document).ready(function () {
+        // slide menu
+        let mobileMenu = $('#side-menu');
 
-    function openSlideMenu() {
-      mobileMenu.style.width = '100%';
-      // mainMenu.style.marginLeft = '250px';
-    }
+        function openSlideMenu() {
+            mobileMenu.style.width = '100%';
+            // mainMenu.style.marginLeft = '250px';
+        }
 
-    function closeSlideMenu() {
-      mobileMenu.style.width = '0';
-      // mainMenu.style.marginLeft = '0';
-    }
+        function closeSlideMenu() {
+            mobileMenu.style.width = '0';
+            // mainMenu.style.marginLeft = '0';
+        }
 
-    $('.open-menu').on('click', function() {
-      // event.preventDefault();
-      openSlideMenu();
-    });
-    $('.btn-close').on('click', function() {
-      // event.preventDefault();
-      closeSlideMenu();
-    });
-  }); // end of document
+        $('.open-menu').on('click', function () {
+            // event.preventDefault();
+            openSlideMenu();
+        });
+        $('.btn-close').on('click', function () {
+            // event.preventDefault();
+            closeSlideMenu();
+        });
+    }); // end of document
 });
