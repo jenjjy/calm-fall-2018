714ae36b0d4226e1ffe090f108e3f3b097d84c70
Nikki Wu <nikkiwu9@gmail.com>
2018-12-10 08:55:25 -0800

    morning push

--- themes/calm/js/custom.js
+++ themes/calm/js/custom.js
@@ -1,30 +1,32 @@
-(function($) {
-  $(function() {
-    console.log('hihi');
-    // slide menu
-    let mobileMenu = $('#side-menu');
+(function ($) {
+    $(function () {
+        console.log('hihi');
+        // slide menu
+        let mobileMenu = $('#side-menu');
 
-    function openSlideMenu() {
-      mobileMenu.style.width = '100%';
-      // mainMenu.style.marginLeft = '250px';
-    }
-    function closeSlideMenu() {
-      mobileMenu.style.width = '0';
-      // mainMenu.style.marginLeft = '0';
-    }
-    $('.open-menu').on('click', function() {
-      console.log('test!');
-      // event.preventDefault();
-      openSlideMenu();
-    });
-    $('.btn-close').on('click', function() {
-      // event.preventDefault();
-      closeSlideMenu();
-    });
+        function openSlideMenu() {
+            mobileMenu.style.width = '100%';
+            // mainMenu.style.marginLeft = '250px';
+        }
 
-    $('#nav-icon').on('click', function() {
-      console.log('working!');
-      $(this).toggleClass('open');
-    });
-  }); // end of doc ready
+        function closeSlideMenu() {
+            mobileMenu.style.width = '0';
+            // mainMenu.style.marginLeft = '0';
+        }
+
+        $('.open-menu').on('click', function () {
+            console.log('test!');
+            // event.preventDefault();
+            openSlideMenu();
+        });
+        $('.btn-close').on('click', function () {
+            // event.preventDefault();
+            closeSlideMenu();
+        });
+
+        $('#nav-icon').on('click', function () {
+            console.log('working!');
+            $(this).toggleClass('open');
+        });
+    }); // end of doc ready
 })(jQuery);
