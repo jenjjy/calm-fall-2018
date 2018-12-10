3332c1b5488edfcb36338ccb069675bca4514426
Nikki Wu <nikkiwu9@gmail.com>
2018-12-10 10:34:53 -0800

    Changed desktop width to 1240px

--- themes/calm/js/custom.js
+++ themes/calm/js/custom.js
@@ -1,26 +1,26 @@
-(function($) {
-  $(function() {
-    // slide menu
-    let mobileMenu = $('#menu-slide');
-    let mobileWidth = $('#page').width();
-    let hamburger = $('#nav-icon');
+(function ($) {
+    $(function () {
+        // slide menu
+        let mobileMenu = $('#menu-slide');
+        let mobileWidth = $('#page').width();
+        let hamburger = $('#nav-icon');
 
-    console.log(mobileWidth);
+        console.log(mobileWidth);
 
-    if (mobileWidth > 600) {
-      console.log('hihihi');
-      mobileMenu.removeClass('menu-slide show');
-      mobileMenu.blur();
-      //   desktopHeader();
-    }
+        if (mobileWidth > 600) {
+            console.log('hihihi');
+            mobileMenu.removeClass('menu-slide show');
+            mobileMenu.blur();
+            //   desktopHeader();
+        }
 
-    hamburger.on('click', function() {
-      $(this).toggleClass('open');
-      mobileMenu.toggleClass('menu-slide show');
-    });
+        hamburger.on('click', function () {
+            $(this).toggleClass('open');
+            mobileMenu.toggleClass('menu-slide show');
+        });
 
-    $('a').click(function() {
-      mobileMenu.removeClass('menu-slide show');
-    });
-  }); // end of doc ready
+        $('a').click(function () {
+            mobileMenu.removeClass('menu-slide show');
+        });
+    }); // end of doc ready
 })(jQuery);
