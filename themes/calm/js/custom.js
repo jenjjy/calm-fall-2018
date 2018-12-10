9fb8afe4aec097cf849e4c5647e3d1e0ad2d5c06
Jennifer <jen.yiu.90@gmail.com>
2018-12-10 13:29:27 -0800

    edit toggle submenu

--- themes/calm/js/custom.js
+++ themes/calm/js/custom.js
@@ -7,12 +7,24 @@
 
     console.log(mobileWidth);
 
-    // if (mobileWidth > 600) {
-    //   console.log('hihihi');
-    //   mobileMenu.removeClass('menu-slide show');
-    //   mobileMenu.blur();
-    //   //   desktopHeader();
-    // }
+    if (mobileWidth <= 600) {
+      event.preventDefault();
+
+      //add prevent default, toggle class, target parent of submenu
+      $('.sub-menu')
+        .parent()
+        .click(function() {
+          $(this).slideToggle('show');
+          console.log('afternoon');
+        });
+      //   $('#primary-menu')
+      //     .children()
+      //     .click(function() {
+      //       $(this)
+      //         .children('.sub-menu')
+      //         .slideToggle('show');
+      //     });
+    }
 
     hamburger.on('click', function() {
       $(this).toggleClass('open');
