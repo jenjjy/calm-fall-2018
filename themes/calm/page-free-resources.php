75ba157338cbffa71670d002af5fff2c84d6a305
Nikki Wu <nikkiwu9@gmail.com>
2018-12-10 16:13:26 -0800

    merge

--- themes/calm/page-free-resources.php
+++ themes/calm/page-free-resources.php
@@ -7,17 +7,31 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        <div class="free-resource-banner">
+            <h1>
+                <?php
+                echo CFS()->get('free_resources_header');
+                ?>
+            </h1>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+            <img src=" <?php
+            echo CFS()->get('free_resources_image');
+            ?>" alt="">
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+            <?php
+            echo CFS()->get('free_resources_button');
+            ?>
+        </div>
+        <?php while (have_posts()) : the_post(); ?>
 
-			<?php endwhile; // End of the loop. ?>
+            <?php get_template_part('template-parts/content', 'page'); ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+        <?php endwhile; // End of the loop. ?>
 
- 
+    </main><!-- #main -->
+</div><!-- #primary -->
+
+
 <?php get_footer(); ?>
