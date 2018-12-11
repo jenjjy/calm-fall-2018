cc6eef4de1d01dd044f1bcb883432bbb3c43104e
Jennifer <jen.yiu.90@gmail.com>
2018-12-10 16:24:15 -0800

    edit js conflict

--- themes/calm/js/custom.js
+++ themes/calm/js/custom.js
@@ -5,56 +5,21 @@
     let mobileWidth = $('#page').width();
     let hamburger = $('#nav-icon');
 
-<<<<<<< HEAD
-
-=======
     console.log(mobileWidth);
->>>>>>> 3c3ff27a967f9005879a35d112f779ba6c22e5e1
 
     if (location.href.indexOf('#') != -1) {
       event.preventDefault();
       console.log('dog');
-      navBar();
+      //   navBar();
     }
 
-    function navBar() {
-      $('.sub-menu')
-        .parent()
-        .click(function() {
-          if (mobileWidth <= 600) {
-            event.preventDefault();
-
-<<<<<<< HEAD
-            //add prevent default, toggle class, target parent of submenu
-            $(this).slideToggle('show');
-            console.log('afternoon');
-          }
-        }); // end of .sub-z
-    }
-=======
-<<<<<<< HEAD
-      //add prevent default, toggle class, target parent of submenu
-      $('.sub-menu')
-        .parent()
-        .click(function () {
-          $(this).slideToggle('show');
-          console.log('afternoon');
-        });
-      //   $('#primary-menu')
-      //     .children()
-      //     .click(function() {
-      //       $(this)
-      //         .children('.sub-menu')
-      //         .slideToggle('show');
-      //     });
-    }
-=======
-          $('.sub-menu').slideToggle('show');
-          console.log('afternoon');
-        }
+    //add prevent default, toggle class, target parent of submenu
+    $('.sub-menu')
+      .parent()
+      .click(function() {
+        $(this).slideToggle('show');
+        console.log('afternoon');
       }); // end of .sub-menu
->>>>>>> 3c3ff27a967f9005879a35d112f779ba6c22e5e1
->>>>>>> 0c0394f20d6ce464644fc7b9f732c96cab18e9cf
 
     hamburger.on('click', function() {
       $(this).toggleClass('open');
