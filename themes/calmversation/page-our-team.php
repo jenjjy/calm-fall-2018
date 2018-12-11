dff67794fccf90d67facefc617a44fc2185b08bd
Nikki Wu <nikkiwu9@gmail.com>
2018-12-11 13:19:36 -0800

    our team video update

--- themes/calmversation/page-our-team.php
+++ themes/calmversation/page-our-team.php
@@ -1,51 +1,65 @@
 <?php
 /**
- * 
+ *
  * Template Name: Our Founder
  * @package Calmversation_Theme
  */
 
 get_header(); ?>
 
-	<div id="primary" class="founder-content-area">
-		<main id="main" class="founder-site-main" role="main">
+<div id="primary" class="founder-content-area">
+    <main id="main" class="founder-site-main" role="main">
 
-		<?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
+        <?php the_title('<h1 class="page-title">', '</h1>'); ?>
 
-		 <?php
-			$args = array(
-			'order' => 'ASC',
-			'post_type'   => 'team_member',
-			'post_status' => 'publish',
-			);
-			
-			$testimonials = new WP_Query( $args );
-			if( $testimonials->have_posts() ) :
-			?>
-			<ul>
-				<?php
-				while( $testimonials->have_posts() ) :
-					$testimonials->the_post();
-					?>
+        <?php
+        $args = array(
+            'order' => 'ASC',
+            'post_type' => 'team_member',
+            'post_status' => 'publish',
+        );
 
-					<li><?php printf( '%1$s - %2$s', get_the_title(), get_the_content() );  ?></li>
+        $testimonials = new WP_Query($args);
+        if ($testimonials->have_posts()) :
+            ?>
+            <ul>
+                <?php
+                while ($testimonials->have_posts()) :
+                    $testimonials->the_post();
+                    ?>
 
-				<?php endwhile; wp_reset_postdata(); ?>
+                    <li><?php printf('%1$s - %2$s', get_the_title(), get_the_content()); ?></li>
 
-			</ul>
+                <?php endwhile;
+                wp_reset_postdata(); ?>
 
-		<?php else :
-			esc_html_e( 'No testimonials in the diving taxonomy!', 'text-domain' );
-			endif;
-			?>
+            </ul>
 
 
+            <div class="tedx-video">
+                <iframe src="  <?php
+                echo CFS()->get('our_team_video');
+                ?>" frameborder="0"></iframe>
+                <h1>
+                    <?php
+                    echo CFS()->get('our_team_header');
+                    ?>
+                </h1>
+                <p>
+                    <?php
+                    echo CFS()->get('our_team_text');
+                    ?>
+                </p>
+            </div>
+
+        <?php else :
+            esc_html_e('No testimonials in the diving taxonomy!', 'text-domain');
+        endif;
+        ?>
 
 
+    </main><!-- #main -->
+</div><!-- #primary -->
 
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
- 
 <?php get_footer(); ?>
