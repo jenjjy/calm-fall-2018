92b2f630fd0dec024b8c97037dcff7b65b539c5c
felix <sparkyxu@hotmail.com>
2018-12-10 16:26:03 -0800

    conflict

--- themes/calm/js/custom.js
+++ themes/calm/js/custom.js
@@ -1,38 +1,33 @@
-(function($) {
-  $(function() {
-    // slide menu
-    let mobileMenu = $('#menu-slide');
-    let mobileWidth = $('#page').width();
-    let hamburger = $('#nav-icon');
+(function ($) {
+  $(function () {
+      // slide menu
+      let mobileMenu = $('#menu-slide');
+      let mobileWidth = $('#page').width();
+      let hamburger = $('#nav-icon');
 
-<<<<<<< HEAD
+      console.log(mobileWidth);
 
-=======
-    console.log(mobileWidth);
->>>>>>> 3c3ff27a967f9005879a35d112f779ba6c22e5e1
+      if (location.href.indexOf('#') != -1) {
+        event.preventDefault();
+        console.log('dog');
+        navBar();
+      }
 
-    if (location.href.indexOf('#') != -1) {
-      event.preventDefault();
-      console.log('dog');
-      navBar();
-    }
+      function navBar() {
+        $('.sub-menu')
+          .parent()
+          .click(function () {
+            if (mobileWidth <= 600) {
+              event.preventDefault();
 
-    function navBar() {
-      $('.sub-menu')
-        .parent()
-        .click(function() {
-          if (mobileWidth <= 600) {
-            event.preventDefault();
 
-<<<<<<< HEAD
-            //add prevent default, toggle class, target parent of submenu
-            $(this).slideToggle('show');
-            console.log('afternoon');
-          }
-        }); // end of .sub-z
-    }
-=======
-<<<<<<< HEAD
+              //add prevent default, toggle class, target parent of submenu
+              $(this).slideToggle('show');
+              console.log('afternoon');
+            }
+          }); // end of .sub-z
+      }
+
       //add prevent default, toggle class, target parent of submenu
       $('.sub-menu')
         .parent()
@@ -48,27 +43,26 @@
       //         .slideToggle('show');
       //     });
     }
-=======
-          $('.sub-menu').slideToggle('show');
-          console.log('afternoon');
-        }
-      }); // end of .sub-menu
->>>>>>> 3c3ff27a967f9005879a35d112f779ba6c22e5e1
->>>>>>> 0c0394f20d6ce464644fc7b9f732c96cab18e9cf
 
-    hamburger.on('click', function() {
-      $(this).toggleClass('open');
-      mobileMenu.toggleClass('menu-slide show');
-    });
+    $('.sub-menu').slideToggle('show'); console.log('afternoon');
+  }
+}); // end of .sub-menu
 
-    $('a').click(function() {
-      mobileMenu.removeClass('menu-slide show');
-    });
 
-    $('.our-company .main-carousel').flickity({
-      cellAlign: 'left',
-      contain: true,
-      wrapAround: true
-    }); //flickity for our-company and our-founder
-  }); // end of doc ready
+hamburger.on('click', function () {
+  $(this).toggleClass('open');
+  mobileMenu.toggleClass('menu-slide show');
+});
+
+$('a').click(function () {
+  mobileMenu.removeClass('menu-slide show');
+});
+
+$('.our-company .main-carousel').flickity({
+cellAlign: 'left',
+contain: true,
+wrapAround: true
+}); //flickity for our-company and our-founder
+}); // end of doc ready
+});
 })(jQuery);
