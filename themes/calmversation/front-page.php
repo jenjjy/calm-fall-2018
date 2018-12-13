2088bc148cb87d18ac69fa465dcd7721f1656e60
Nikki Wu <nikkiwu9@gmail.com>
2018-12-13 10:03:35 -0800

    add max width variables

--- themes/calmversation/front-page.php
+++ themes/calmversation/front-page.php
@@ -34,138 +34,139 @@ get_header(); ?>
 
         </div>
 
+        <div class="container">
 
-        <!--- CONTENT -->
-        <?php /* Start the Loop */ ?>
-        <?php while (have_posts()) :
-            the_post(); ?>
+            <!--- CONTENT -->
+            <?php /* Start the Loop */ ?>
+            <?php while (have_posts()) :
+                the_post(); ?>
 
-            <?php get_template_part('template-parts/content'); ?>
-            <!--- FOUNDER -->
-            <div class="founder-container">
-                <iframe src=" <?php
-                echo CFS()->get('listen_to_founder_video');
-                ?>" frameborder="0"></iframe>
-                <div class="founder-text-container">
+                <?php get_template_part('template-parts/content'); ?>
+                <!--- FOUNDER -->
+                <div class="founder-container">
+                    <iframe src=" <?php
+                    echo CFS()->get('listen_to_founder_video');
+                    ?>" frameborder="0"></iframe>
+                    <div class="founder-text-container">
+                        <h1>
+                            <?php
+                            echo CFS()->get('listen_to_founder_header');
+                            ?>
+                        </h1>
+                        <?php
+                        echo CFS()->get('listen_to_founder_button');
+                        ?>
+                    </div>
+
+                </div>
+                <!--- SOLUTIONS -->
+                <div class="solutions-container">
                     <h1>
                         <?php
-                        echo CFS()->get('listen_to_founder_header');
+                        echo CFS()->get('game_changing_solution_header');
                         ?>
                     </h1>
-                    <?php
-                    echo CFS()->get('listen_to_founder_button');
-                    ?>
-                </div>
-
-            </div>
-            <!--- SOLUTIONS -->
-            <div class="solutions-container">
-                <h1>
-                    <?php
-                    echo CFS()->get('game_changing_solution_header');
-                    ?>
-                </h1>
-                <div>
-                    <img src=" <?php
-                    echo CFS()->get('game_changing_solution_image');
-                    ?>" alt="game changing solution">
-                    <p class="solution-text-mobile">
-                        <?php
-                        echo CFS()->get('game_changing_solution_text');
-                        ?>
-                    </p>
-                    <p class="solution-text-desktop">
-                        <?php
-                        echo CFS()->get('game_changing_solution_text_desktop');
-                        ?>
-                    </p>
-                </div>
-            </div>
-
-            <!--- GOALS  MOBILE -->
-        <?php endwhile; ?>
-        <div class="checkmark-container">
-            <?php
-
-            $loop = CFS()->get('goal');
-            foreach ($loop as $row) : ?>
-                <div class="front-page-check-mark">
-                    <img src="<?php echo $row['goal_image']; ?>" alt="">
-                    <h2><?php echo $row['goal_text']; ?></h2>
-                </div>
-            <?php endforeach; ?>
-
-        </div>
-
-        <!--- GOALS DESKTOP -->
-        <div class="goals-desktop">
-            <?php
-            $loop = CFS()->get('goals_desktop');
-            foreach ($loop as $row) : ?>
-                <div class="goals-boxes">
-                    <div class="goals-text">
-                        <h2><?php echo $row['goals_header']; ?></h2>
-                        <p><?php echo $row['goals_text_desktop']; ?></p>
+                    <div>
+                        <img src=" <?php
+                        echo CFS()->get('game_changing_solution_image');
+                        ?>" alt="game changing solution">
+                        <p class="solution-text-mobile">
+                            <?php
+                            echo CFS()->get('game_changing_solution_text');
+                            ?>
+                        </p>
+                        <p class="solution-text-desktop">
+                            <?php
+                            echo CFS()->get('game_changing_solution_text_desktop');
+                            ?>
+                        </p>
                     </div>
                 </div>
-            <?php endforeach; ?>
-        </div>
 
-
-		<?php get_template_part( 'template-parts/testimony' ); ?>
-
-        <!--- COURSES -->
-        <div class="view-courses">
-            <?php
-            echo CFS()->get('view_courses');
-            ?>
-        </div>
-
-        <!--- CAMPAIGN -->
-        <div class="campaign-container">
-            <h2>
+                <!--- GOALS  MOBILE -->
+            <?php endwhile; ?>
+            <div class="checkmark-container">
                 <?php
-                echo CFS()->get('campaign_header');
-                ?>
-            </h2>
 
-            <div class="campaign-logo-container-mobile">
+                $loop = CFS()->get('goal');
+                foreach ($loop as $row) : ?>
+                    <div class="front-page-check-mark">
+                        <img src="<?php echo $row['goal_image']; ?>" alt="">
+                        <h2><?php echo $row['goal_text']; ?></h2>
+                    </div>
+                <?php endforeach; ?>
+
+            </div>
+
+            <!--- GOALS DESKTOP -->
+            <div class="goals-desktop">
+                <?php
+                $loop = CFS()->get('goals_desktop');
+                foreach ($loop as $row) : ?>
+                    <div class="goals-boxes">
+                        <div class="goals-text">
+                            <h2><?php echo $row['goals_header']; ?></h2>
+                            <p><?php echo $row['goals_text_desktop']; ?></p>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+
+
+            <?php get_template_part('template-parts/testimony'); ?>
+
+            <!--- COURSES -->
+            <div class="view-courses">
+                <?php
+                echo CFS()->get('view_courses');
+                ?>
+            </div>
+
+            <!--- CAMPAIGN -->
+            <div class="campaign-container">
+                <h2>
+                    <?php
+                    echo CFS()->get('campaign_header');
+                    ?>
+                </h2>
+
+                <div class="campaign-logo-container-mobile">
+                    <img src="<?php
+                    echo CFS()->get('campaign_logo');
+                    ?>" alt="" class="campaign-logo">
+                </div>
+
+                <p class="campaign-text-mobile">
+                    <?php
+                    echo CFS()->get('campaign_text');
+                    ?>
+                </p>
+
+                <p class="campaign-text-desktop">
+                    <?php
+                    echo CFS()->get('campaign_text_desktop');
+                    ?>
+                </p>
+
+                <div class="campaign-button">
+                    <?php
+                    echo CFS()->get('campaign_button');
+                    ?>
+                </div>
+            </div>
+
+            <div class="campaign-logo-container-desktop">
                 <img src="<?php
                 echo CFS()->get('campaign_logo');
                 ?>" alt="" class="campaign-logo">
             </div>
+            <?php /* Start Testimony Loop */ ?>
+            <?php while (have_posts()) : the_post(); ?>
 
-            <p class="campaign-text-mobile">
-                <?php
-                echo CFS()->get('campaign_text');
-                ?>
-            </p>
 
-            <p class="campaign-text-desktop">
-                <?php
-                echo CFS()->get('campaign_text_desktop');
-                ?>
-            </p>
+            <?php endwhile; ?>
 
-            <div class="campaign-button">
-                <?php
-                echo CFS()->get('campaign_button');
-                ?>
-            </div>
         </div>
-
-        <div class="campaign-logo-container-desktop">
-            <img src="<?php
-            echo CFS()->get('campaign_logo');
-            ?>" alt="" class="campaign-logo">
-        </div>
-        <?php /* Start Testimony Loop */ ?>
-        <?php while (have_posts()) : the_post(); ?>
-
-
-        <?php endwhile; ?>
-
-
     </main><!-- #main -->
 </div><!-- #primary -->
 
