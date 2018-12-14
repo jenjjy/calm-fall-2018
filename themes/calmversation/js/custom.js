ab4364305fb3a3382844fd889fa3205abb3aa165
Nikki Wu <nikkiwu9@gmail.com>
2018-12-13 16:41:19 -0800

    cusotm js update

--- themes/calmversation/js/custom.js
+++ themes/calmversation/js/custom.js
@@ -1,112 +1,98 @@
-(function($) {
-  $(function() {
-    // slide menu
-    let mobileMenu = $('#menu-slide');
-    let mobileWidth = $('#page').width();
-    let hamburger = $('#nav-icon');
-    let subMenu = $('.sub-menu');
+(function ($) {
+    $(function () {
+        // slide menu
+        let mobileMenu = $('#menu-slide');
+        let mobileWidth = $('#page').width();
+        let hamburger = $('#nav-icon');
+        let subMenu = $('.sub-menu');
 
-    // Sub-Menu toggle
+        // Sub-Menu toggle
 
-    $('.menu-item a').on('click', function(event) {
-      let $select = $(this);
-      let hasLink = $select.attr('href');
+        $('.menu-item a').on('click', function (event) {
+            let $select = $(this);
+            let hasLink = $select.attr('href');
 
-      if (hasLink === '#' && mobileWidth < 600) {
-        event.preventDefault();
-        // return subMenuToggle();
+            if (hasLink === '#' && mobileWidth < 600) {
+                event.preventDefault();
+                // return subMenuToggle();
 
-        subMenu.slideToggle('fast');
-        $select.addClass('about-us-border');
-        $select.parent().addClass('about-us-padding');
-        // $select.blur().removeClass('about-us-border');
-        console.log('false');
-        // return false;
-      } else {
-        return true;
-      }
-    }); // end of sub-menu toggle
-    // subMenu.on('blur', function(){
-    //   $select.removeClass('about-us-border');
-    // }
+                subMenu.slideToggle('fast');
+                $select.addClass('about-us-border');
+                $select.parent().addClass('about-us-padding');
+                // $select.blur().removeClass('about-us-border');
+                console.log('false');
+                // return false;
+            } else {
+                return true;
+            }
+        }); // end of sub-menu toggle
+        // subMenu.on('blur', function(){
+        //   $select.removeClass('about-us-border');
+        // }
 
-    hamburger.on('click', function() {
-      $(this).toggleClass('open');
-      mobileMenu.toggleClass('menu-slide show');
-    });
-
-    if (mobileWidth > 600) {
-      $('.testimony-carousel').flickity({
-        cellAlign: 'left',
-        contain: true,
-        wrapAround: true
-        // autoPlay: 8000
-      });
-    } else {
-      $('.testimony-carousel').flickity({
-        cellAlign: 'left',
-        contain: true,
-        wrapAround: true,
-        prevNextButtons: false
-        // autoPlay: 8000
-      });
-    } //carousel testimony
-
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-    if (
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        navigator.userAgent
-      )
-    ) {
-      $('.main-carousel').flickity({
-        cellAlign: 'left',
-        contain: true,
-        wrapAround: true
-      }); //flickity for our-company & our-founder
-    }
-
-    //course-page readmore
-    let extraText = $('.course-text-more');
-    let readMore = $('.readmore .wp-block-button__link');
-
-    readMore.click(function() {
-      console.log('asdf');
-      extraText.toggleClass('hide-text');
-    });
-=======
-
-    $(window).resize(function () {
-=======
-    $(window).resize(function() {
->>>>>>> f8ad1f30b9772943aabcd1cc07a8baf251c8b716
-      if ($(window).width() < 600) {
-        $('.main-carousel').flickity({
-          cellAlign: 'left',
-          contain: true,
-          wrapAround: true
+        hamburger.on('click', function () {
+            $(this).toggleClass('open');
+            mobileMenu.toggleClass('menu-slide show');
         });
-      }
-    }); //flickity for our-company & our-founder
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+        if (mobileWidth > 600) {
+            $('.testimony-carousel').flickity({
+                cellAlign: 'left',
+                contain: true,
+                wrapAround: true
+                // autoPlay: 8000
+            });
+        } else {
+            $('.testimony-carousel').flickity({
+                cellAlign: 'left',
+                contain: true,
+                wrapAround: true,
+                prevNextButtons: false
+                // autoPlay: 8000
+            });
+        } //carousel testimony
 
 
+        if (
+            /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+                navigator.userAgent
+            )
+        ) {
+            $('.main-carousel').flickity({
+                cellAlign: 'left',
+                contain: true,
+                wrapAround: true
+            }); //flickity for our-company & our-founder
+        }
 
->>>>>>> cfb883b596a284ac4217ab6d2c5aff58513eb29d
-=======
->>>>>>> f8ad1f30b9772943aabcd1cc07a8baf251c8b716
-=======
+        //course-page readmore
+        let extraText = $('.course-text-more');
+        let readMore = $('.readmore .wp-block-button__link');
 
-    //course-page readmore
-    let extraText = $('.course-text-more');
-    let readMore = $('.readmore .wp-block-button__link');
+        readMore.click(function () {
+            console.log('asdf');
+            extraText.toggleClass('hide-text');
+        });
 
-    readMore.click(function() {
-      console.log('asdf');
-      extraText.toggleClass('hide-text');
-    });
->>>>>>> c9dae1cf2e738c5407f3150b26161e0bb60a7e50
-  }); // end of doc ready
-})(jQuery);
+        $(window).resize(function () {
+            $(window).resize(function () {
+                if ($(window).width() < 600) {
+                    $('.main-carousel').flickity({
+                        cellAlign: 'left',
+                        contain: true,
+                        wrapAround: true
+                    });
+                }
+            }); //flickity for our-company & our-founder
+
+
+            //course-page readmore
+            let extraText = $('.course-text-more');
+            let readMore = $('.readmore .wp-block-button__link');
+
+            readMore.click(function () {
+                console.log('asdf');
+                extraText.toggleClass('hide-text');
+            });
+        }); // end of doc ready
+    })(jQuery);
