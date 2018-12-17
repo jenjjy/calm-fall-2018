e32306d4825887be543cf5e8609c7b6191ae9963
Nikki Wu <nikkiwu9@gmail.com>
2018-12-17 14:05:57 -0800

    fix campaign

--- themes/calmversation/page-our-campaign.php
+++ themes/calmversation/page-our-campaign.php
@@ -6,31 +6,32 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        <div class="site-container">
+            <?php while (have_posts()) : the_post(); ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+                <?php get_template_part('template-parts/content', 'page'); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+            <?php endwhile; // End of the loop. ?>
+            <div class="campaign-features-wraper">
+                <?php
+                $loop = CFS()->get('campaign');
+                foreach ($loop as $row) : ?>
+                    <div class="campaign-features">
+                        <img src="<?php echo $row['campaign_feature_icon']; ?>">
+                        <div class="campaign-text">
+                            <h2><?php echo $row['campaign_feature_title']; ?></h2>
+                            <p>
+                                <?php echo $row['campaign_feature_text']; ?>
+                            </p>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </main><!-- #main -->
+</div><!-- #primary -->
 
-			<?php endwhile; // End of the loop. ?>
-		<div class="campaign-features-wraper">
-			<?php
-				$loop = CFS()->get( 'campaign' );
-				foreach ( $loop as $row ) : ?>
-					<div class="campaign-features">
-						<img src="<?php echo $row['campaign_feature_icon']; ?>">
-						<div class="campaign-text">
-							<h2><?php echo $row['campaign_feature_title']; ?></h2>
-							<p> 
-								<?php echo $row['campaign_feature_text']; ?>
-							</p>
-						</div>
-					</div>
-				<?php endforeach; ?> 
-		</div>
-		</main><!-- #main -->
-	</div><!-- #primary -->
 
- 
 <?php get_footer(); ?>
