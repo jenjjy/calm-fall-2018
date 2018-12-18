0a8c901c86763dc01f0c4b72698f03d75bce3fbb
Nikki Wu <nikkiwu9@gmail.com>
2018-12-18 09:29:11 -0800

    update front-page

--- themes/calmversation/js/custom.js
+++ themes/calmversation/js/custom.js
@@ -1,77 +1,77 @@
-(function($) {
-  $(function() {
-    // slide menu
-    let mobileMenu = $('#menu-slide');
-    let mobileWidth = $('#page').width();
-    let hamburger = $('#nav-icon');
-    let subMenu = $('.sub-menu');
+(function ($) {
+    $(function () {
+        // slide menu
+        let mobileMenu = $('#menu-slide');
+        let mobileWidth = $('#page').width();
+        let hamburger = $('#nav-icon');
+        let subMenu = $('.sub-menu');
 
-    // Sub-Menu toggle
-    $('.menu-item a').on('click', function(event) {
-      let $select = $(this);
-      let hasLink = $select.attr('href');
+        // Sub-Menu toggle
+        $('.menu-item a').on('click', function (event) {
+            let $select = $(this);
+            let hasLink = $select.attr('href');
 
-      if (hasLink === '#' && mobileWidth < 600) {
-        event.preventDefault();
+            if (hasLink === '#' && mobileWidth < 600) {
+                event.preventDefault();
 
-        subMenu.slideToggle('fast');
-        $select.toggleClass('about-us-border');
-        subMenu.parent().toggleClass('about-us-padding');
-      } else {
-        return true;
-      }
-    }); // end of sub-menu toggle
+                subMenu.slideToggle('fast');
+                $select.toggleClass('about-us-border');
+                subMenu.parent().toggleClass('about-us-padding');
+            } else {
+                return true;
+            }
+        }); // end of sub-menu toggle
 
-    hamburger.on('click', function() {
-      $(this).toggleClass('open');
-      mobileMenu.toggleClass('menu-slide show');
-    });
-
-    if (mobileWidth > 600) {
-      $('.testimony-carousel').flickity({
-        cellAlign: 'left',
-        contain: true,
-        wrapAround: true,
-        autoPlay: 8000
-      });
-    } else {
-      $('.testimony-carousel').flickity({
-        cellAlign: 'left',
-        contain: true,
-        wrapAround: true,
-        prevNextButtons: false,
-        autoPlay: 8000
-      });
-    } //carousel testimony
-
-    $(window).resize(function() {
-      // console.log('carousel');
-      if ($(window).width() < 600) {
-        $('.main-carousel').flickity({
-          cellAlign: 'left',
-          contain: true,
-          wrapAround: true
+        hamburger.on('click', function () {
+            $(this).toggleClass('open');
+            mobileMenu.toggleClass('menu-slide show');
         });
-      }
-    }); //flickity for our-company & our-founder
 
-    //course-page readmore
-    let extraText = $('.course-text-more');
-    let readMore = $('.readmore .wp-block-button__link');
+        if (mobileWidth > 600) {
+            $('.testimony-carousel').flickity({
+                cellAlign: 'left',
+                contain: true,
+                wrapAround: true,
+                autoPlay: 8000
+            });
+        } else {
+            $('.testimony-carousel').flickity({
+                cellAlign: 'left',
+                contain: true,
+                wrapAround: true,
+                prevNextButtons: false,
+                autoPlay: 8000
+            });
+        } //carousel testimony
 
-    readMore.click(function() {
-      // console.log('asdf');
-      extraText.toggleClass('hide-text');
-    });
+        $(window).resize(function () {
+            // console.log('carousel');
+            if ($(window).width() < 600) {
+                $('.main-carousel').flickity({
+                    cellAlign: 'left',
+                    contain: true,
+                    wrapAround: true
+                });
+            }
+        }); //flickity for our-company & our-founder
 
-    $('.bubble').on('click', function() {
-      $('.bubble').removeClass('active');
-      $(this).addClass('active');
-      let id = $(this).attr('data-id');
-      // console.log(id);
-      $('.themes').hide();
+        //course-page readmore
+        let extraText = $('.course-text-more');
+        let readMore = $('.readmore .wp-block-button__link');
 
-      $(`[data-id=${id}]`).show();
-    });
-  }); // end of doc ready
+        readMore.click(function () {
+            // console.log('asdf');
+            extraText.toggleClass('hide-text');
+        });
+
+        $('.bubble').on('click', function () {
+            $('.bubble').removeClass('active');
+            $(this).addClass('active');
+            let id = $(this).attr('data-id');
+            // console.log(id);
+            $('.themes').hide();
+
+            $(`[data-id=${id}]`).show();
+        });
+    }); // end of doc ready
 })(jQuery);
