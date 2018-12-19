6c3420f273a941a2a1f3870fb29b6d4787349d06
Nikki Wu <nikkiwu9@gmail.com>
2018-12-19 11:00:05 -0800

    update our company

--- themes/calmversation/page-our-company.php
+++ themes/calmversation/page-our-company.php
@@ -6,17 +6,50 @@
 
 get_header(); ?>
 
-	<div id="primary" class="company-content-area">
-		<main id="main" class="company-site-main" role="main">
-			<div class="site-container">
-					<?php while ( have_posts() ) : the_post(); ?>
+<div id="primary" class="company-content-area">
+    <main id="main" class="company-site-main" role="main">
+        <div class="site-container">
+            <?php while (have_posts()) : the_post(); ?>
 
-						<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-					<?php endwhile; // End of the loop. ?>
-			</div>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+                <?php get_template_part('template-parts/content', 'page'); ?>
 
- 
+                <div class="our-founder-container">
+                    <img src=" <?php
+                    echo CFS()->get('our_image');
+                    ?>" alt="">
+                    <h2>
+                        <?php
+                        echo CFS()->get('our_header');
+                        ?>
+                    </h2>
+                    <p>
+                        <?php
+                        echo CFS()->get('our_text');
+                        ?>
+                    </p>
+
+                </div>
+
+
+                <div class="main-carousel">
+                    <?php
+                    $loop = CFS()->get('our_goals');
+                    foreach ($loop as $row) : ?>
+                        <div class="carousel-cell">
+                            <img src="<?php echo $row['our_goals_image']; ?>" alt="">
+                            <h2><?php echo $row['our_goals_header']; ?></h2>
+                            <p><?php echo $row['our_goals_text']; ?></p>
+                        </div>
+                    <?php endforeach; ?>
+
+
+                </div>
+
+            <?php endwhile; // End of the loop. ?>
+        </div>
+    </main><!-- #main -->
+</div><!-- #primary -->
+
+
 <?php get_footer(); ?>
--- themes/calmversation/page-our-founder.php
+++ themes/calmversation/page-our-founder.php
@@ -1,25 +1,59 @@
 <?php
 /**
- * 
+ *
  * Template Name: Our Founder
  * @package Calmversation_Theme
  */
 
 get_header(); ?>
 
-	<div id="primary" class="founder-content-area">
-	   <div class="site-container">
-		    <main id="main" class="founder-site-main" role="main">
+<div id="primary" class="founder-content-area">
+    <div class="site-container">
+        <main id="main" class="founder-site-main" role="main">
+            <div class="site-container">
+                <?php while (have_posts()) : the_post(); ?>
 
-			 	<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php get_template_part( 'template-parts/content', 'page' ); ?>
+                    <?php get_template_part('template-parts/content', 'page'); ?>
 
-				<?php endwhile; // End of the loop. ?>
+                    <div class="our-founder-container">
+                        <img src=" <?php
+                        echo CFS()->get('our_image');
+                        ?>" alt="">
+                        <div class="our-founder-info">
+                            <h2>
+                                <?php
+                                echo CFS()->get('our_header');
+                                ?>
+                            </h2>
+                            <p>
+                                <?php
+                                echo CFS()->get('our_text');
+                                ?>
+                            </p>
+                        </div>
+                    </div>
 
-			</main><!-- #main -->
-		</div>
-	</div><!-- #primary -->
 
- 
+                    <div class="main-carousel">
+                        <?php
+                        $loop = CFS()->get('our_goals');
+                        foreach ($loop as $row) : ?>
+                            <div class="carousel-cell">
+                                <img src="<?php echo $row['our_goals_image']; ?>" alt="">
+                                <h2><?php echo $row['our_goals_header']; ?></h2>
+                                <p><?php echo $row['our_goals_text']; ?></p>
+                            </div>
+                        <?php endforeach; ?>
+
+
+                    </div>
+
+                <?php endwhile; // End of the loop. ?>
+            </div>
+        </main><!-- #main -->
+    </div>
+</div><!-- #primary -->
+
+
 <?php get_footer(); ?>
